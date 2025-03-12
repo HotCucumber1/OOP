2a458103157a231b68ac19022b0e9b1c57872917
Dmitriy <shadowpersik05@mail.ru>
2025-03-13 00:38:58 +0300

    Переименовал названия тестов

--- lab2/Dictionary/Dictionary.hpp
+++ lab2/Dictionary/Dictionary.hpp
@@ -6,7 +6,7 @@
 
 using Dictionary = std::unordered_map<std::string, std::set<std::string>>;
 
-
+// TODO: выделить Core
 std::string GetFileName(int argc, char* argv[]);
 Dictionary ReadDict(const std::string& dictFileName);
 void StartDictionary(Dictionary& dict, const std::string& dictFileName);
